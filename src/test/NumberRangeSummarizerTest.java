c10f91ea30b38196fbe988a6536bd66f7407094e
Sihle Calana <CLNSIH001@myuct.ac.za>
2024-06-05 21:03:00 +0200

    created Tests for Integer and Long numbers

--- src/test/NumberRangeSummarizerTest.java
+++ src/test/NumberRangeSummarizerTest.java
@@ -5,21 +5,38 @@ import numberrangesummarizer.NumberRangeSummarizerImpl;
 import org.junit.Test;
 import org.junit.Assert;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Tests the NumberRangeSummarizer
+ * @author sihle
+ * */
 public class NumberRangeSummarizerTest {
 
     @Test
-    public void collect() {
-        NumberRangeSummarizer nrs = new NumberRangeSummarizerImpl();
-        String expected = "";
-        String actual = "";
+    public void collectTest() {
+        NumberRangeSummarizer<Integer> nrs = new NumberRangeSummarizerImpl<>();
+        Collection<Integer> expected = Arrays.asList(1,3,6,7,8,12,13,14,15,21,22,23,24,31);
+        Collection<Integer> actual = nrs.collect("1,3,6,7,8,12,13,14,15,21,22,23,24,31");
         Assert.assertEquals(expected,actual);
     }
 
     @Test
-    public void summarizeCollection() {
-        NumberRangeSummarizer nrs = new NumberRangeSummarizerImpl();
-        String expected = "";
-        String actual = "";
+    public void summarizeCollectionTest() {
+        NumberRangeSummarizer<Integer> nrs = new NumberRangeSummarizerImpl<>();
+        Collection<Integer> input = Arrays.asList(1,3,6,7,8,12,13,14,15,21,22,23,24,31);
+        String expected = "1, 3, 6-8, 12-15, 21-24, 31";
+        String actual = nrs.summarizeCollection(input);
         Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void longRangeSummarizerTest() {
+        NumberRangeSummarizer<Long> nrs = new NumberRangeSummarizerImpl<>();
+        Collection<Long> input = Arrays.asList(1L,3L,2L,5L,7L,10L,8L);
+        String expected = "1, 2-3, 5, 7-8, 10";
+        String actual = nrs.summarizeCollection(input);
+        Assert.assertEquals(expected, actual);
     }
 }
